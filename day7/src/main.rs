3eefdd100649f38c94c844d52dbeb0e2992ce4a1
CommanderKronos <yuriwit0@gmail.com>
2023-12-09 15:03:43 +0100

    Day7 part 1 complete. FUN!

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,9 +1,9 @@
-use std::{fs, time::Instant, collections::{HashSet, HashMap}};
+use std::{fs, time::Instant, collections::HashMap};
 
 fn main() {
     let start = Instant::now();
 
-    let file_name = String::from("type_test.txt");
+    let file_name = String::from("input.txt");
     let file_content: String = match fs::read_to_string(file_name) {
         Ok(content) => content,
         Err(error) => panic!("Error opening the file. Error: {:?}", error),
@@ -15,10 +15,19 @@ fn main() {
         all_hands.push(Hand::new(card_str, bid_str))
     }
 
+    // Sort by card values first
+    all_hands.sort_by(|hand1, hand2| hand1.card_values.cmp(&hand2.card_values));
 
-    for hand in all_hands {
-        println!("{:?}", hand)
+    // Sort by hand_type value
+    all_hands.sort_by(|hand1, hand2| hand1.hand_type.value().cmp(&hand2.hand_type.value()));
+
+    let mut total_winnings: u32 = 0;
+    for (rank, hand) in all_hands.iter().enumerate() {
+        total_winnings += hand.bid * (rank as u32 + 1);
+        println!("Rank: {:?}, Hand: {:?}", rank+1, hand)
     }
+
+    println!("Total_winnings: {:?}", total_winnings);
 
     println!("Time elapsed: {:?}", start.elapsed())
 }
@@ -59,6 +68,24 @@ fn check_type(cards: Vec<char>) -> HandType {
   
 }
 
+fn check_card_value(cards: Vec<char>) -> Vec<u32> {
+    let mut value_vec: Vec<u32> = Vec::new();
+    for card in cards {
+        value_vec.push(
+            match card {
+                'A' => 14,
+                'K' => 13,
+                'Q' => 12,
+                'J' => 11,
+                'T' => 10,
+                _ => card.to_digit(10).unwrap()
+            }
+        )
+    }
+
+    value_vec
+}
+
 #[derive(Debug)]
 enum HandType {
     Kind5,
@@ -70,9 +97,24 @@ enum HandType {
     High
 }
 
+impl HandType {
+    const fn value(&self) -> u32 {
+        match *self {
+            HandType::Kind5 => 7,
+            HandType::Kind4 => 6,
+            HandType::FullH => 5,
+            HandType::Kind3 => 4,
+            HandType::Pair2 => 3,
+            HandType::Pair1 => 2,
+            HandType::High => 1,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Hand {
     cards: Vec<char>,
+    card_values: Vec<u32>,
     hand_type: HandType,
     bid: u32
 }
@@ -81,8 +123,8 @@ impl Hand {
     pub fn new(card_str: &str, bid_str: &str) -> Self {
         let card_vec: Vec<char> = card_str.chars().collect();
         let hand_type = check_type(card_vec.clone());
-        // let hand_type = HandType::FullH;
+        let card_values = check_card_value(card_vec.clone());
 
-        Self { cards: card_vec, hand_type, bid: bid_str.parse().unwrap() }
+        Self { cards: card_vec, card_values, hand_type, bid: bid_str.parse().unwrap() }
     }
 }
